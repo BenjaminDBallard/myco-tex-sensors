f196ff89302f2281623b304ee42a1485d2edde3c
Ben Ballard <benb971@gmail.com>
2024-01-28 16:45:18 -0600

    added post req and tested esp

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 # MycoTex Sensors
 
 ## Summary
+
 This project holds the scripts for esp32 microcomputers that recieve and send the data from their corosponding sensors for
 [Myco-tex] (url tbd).
 This site provides:
@@ -18,43 +19,50 @@ Current work is focused on establishing a monitoring system and database for tem
 
 ## Related Projects/Modules
 
-**myco-tex-sensors** (https://github.com/BenjaminDBallard/myco-tex-sensors)  (This project) 
+**myco-tex-sensors** (https://github.com/BenjaminDBallard/myco-tex-sensors) (This project)
+
 - Contains scripts to effectively recieve and send temps, humitity, co2 and other enviromental variables using C++ with the Arduino IDE.
 
-**myco-tex-backend** (https://github.com/BenjaminDBallard/myco-tex-backend)  
+**myco-tex-backend** (https://github.com/BenjaminDBallard/myco-tex-backend)
+
 - Serves up REST endpoints for dashboard application using express, node, & mysql2. Authorization and security for the API use JWT and Bcrypt.
 
-**myco-tex-dashboard** (https://github.com/jason-cornish/myco-tex-dashboard)  
+**myco-tex-dashboard** (https://github.com/jason-cornish/myco-tex-dashboard)
+
 - This is the main dashboard application used for monitoring facility enviromental status with React & Styled-Components.
 
 <br/>
 <hr/>
 
 ## Development
+
 **Main**
+
 - [x] Set up Arduino IDE
 - [x] Write test function
 - [x] Test Build and flash
 - [x] Wire probe to ESP32
 - [x] Write function to connect to WIFI
 - [x] Write function to get sensor measurement
-- [ ] write function to send https post request
-- [ ] Build and flash esp
+- [x] write function to send https post request
+- [x] Build and flash esp
 
 **Final**
-- [ ] Test live ESP32
+
+- [x] Test live ESP32
 
 <br/>
 <hr/>
 
 # Project Setup
+
 > [!TIP]
 >
 > Install Arduino Extension in VSCode (follow install directions)
-> 
+>
 > cd into the folder for the probetype you intend to install
 >
-> select your board type and Serial port# 
+> select your board type and Serial port#
 >
 > then build and flash the ESP32
 >
--- probeTherm/probeTherm.ino
+++ probeTherm/probeTherm.ino
@@ -1,51 +1,87 @@
-#include <WiFi.h>
+#include <ArduinoJson.h>
+// #include <WiFi.h>
+#include <WiFiMulti.h>
 #include <HTTPClient.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// user config: TODO
-const char WIFI_SSID[] = "ssd"; // CHANGE IT
-const char WIFI_PASSWORD[] = "pass"; // CHANGE IT
+// WIFI VALUES
+const char *AP_SSID = "Ben and Kelbie"; // CHANGE IT
+const char *AP_PWD = "QueenWilla22"; // CHANGE IT
+String EDP_URL = "http://ptsv3.com/t/esptest/post/json/";
 
-//DS18B20 config
+//DEVICE VALUES
+String controllerId = "1234567890";
+String devicePass = "password";
+
+//MEASUREMENT VALUES
+float tempC; // temperature in Celsius
+float tempF; // temperature in Fahrenheit
+
+//DB MEASUREMENT COLLUMN NAME
+String COL = "probe_therm_measure";
+
+
+
+WiFiMulti wifiMulti;
+
+// DS18B20 config
 #define SENSOR_PIN  17 // ESP32 pin GPIO17 connected to DS18B20 sensor's DATA pin
 
 OneWire oneWire(SENSOR_PIN);
 DallasTemperature DS18B20(&oneWire);
-float tempC; // temperature in Celsius
-float tempF; // temperature in Fahrenheit
 
 void setup() {
- // open serial
   Serial.begin(9600);
-  Serial.println("*****************************************************");
-  Serial.println("********** Program Start : Connect ESP32 Therm to DB.");
-  Serial.println("Wait for WiFi... ");
 
-  // connecting to the WiFi network
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.println("Connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  // connected
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
+  delay(5000);
+  wifiMulti.addAP(AP_SSID, AP_PWD);
 
-  // initialize device.
   DS18B20.begin();    // initialize the DS18B20 sensor
 }
 
 void loop() {
-  // Print temperature sensor details.
   DS18B20.requestTemperatures();       // send the command to get temperatures
   tempC = DS18B20.getTempCByIndex(0);  // read temperature in °C
   tempF = tempC * 9 / 5 + 32; // convert °C to °F
-  Serial.print("probe_therm_measure: ");
-  Serial.println(tempF);    // print the temperature in °F
+  postDataToServer();
+}
 
+void postDataToServer() {
+ 
+  Serial.println("Checking wifi connection...");
+  // Block until we are able to connect to the WiFi access point
+  if (wifiMulti.run() == WL_CONNECTED) {
+     
+    Serial.println("Posting JSON data to server...");
 
-  delay(500);    // delay
+    HTTPClient http;   
+     
+    http.begin(EDP_URL);  
+    http.addHeader("Content-Type", "application/json");         
+     
+    StaticJsonDocument<200> doc;
+    // Add values in the document
+    //
+    doc["controller_id"] = controllerId;
+    doc["device_pass"] = devicePass;
+    doc[COL] = tempF;
+     
+    String requestBody;
+    serializeJson(doc, requestBody);
+     
+    int httpResponseCode = http.POST(requestBody);
+ 
+    if(httpResponseCode>0){
+       
+      String response = http.getString();                       
+       
+      Serial.println(httpResponseCode);   
+      Serial.println(response);
+      delay(1000);
+    } else {
+      Serial.printf("Error occurred while sending HTTP POST: %s\n", http.errorToString(httpResponseCode).c_str());
+    }
+     
+  }
 }
